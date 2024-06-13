3418c4112daca2500e9a3b83bc0e2ec8a601525c
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-06-13 11:22:59 -0600

    REFACTORING some methods

--- MetaPy/DifferentialEvolution/DifferentialEvolution.py
+++ MetaPy/DifferentialEvolution/DifferentialEvolution.py
@@ -4,14 +4,9 @@ class DifferentialEvolution:
             Class for Differential Evolution Metaheuristic
             -- objectiveFunction : Function being optimized 
             -- initializeIndividual : Function to create individuals
-
-            -- population : Solutions population
-            -- fitnessValuesPopulation : Fitness values population
         """
         self.objectiveFunction = objectiveFunction
         self.initializeIndividual = initializeIndividual
-        self.population = []
-        self.fitnessValuesPopulation = []
 
     def __call__(self,iterations:int,populationSize:int,scalingFactor:float,crossoverRate:float):
         """
@@ -21,32 +16,42 @@ class DifferentialEvolution:
             -- scalingFactor : Parameter F. Scaling factor for difference vector 
             -- crossoverRate : Parameter Cr. Crossover rate for crossover operation
         """
-        objectiveFunction = self.objectiveFunction
-        self.population = [self.initializeIndividual() for _ in range(populationSize)]
-        self.fitnessValuesPopulation = [objectiveFunction(*individual) for individual in self.population]
+        self.populationSize = populationSize
+        self.scalingFactor = scalingFactor
+        self.crossoverRate = crossoverRate
+        self.diffevol_InitializePopulation()
         for iteration in range(iterations):
-            for indexIndividual in range(populationSize):
-                mutantIndividual = self._differentialMutantOperation(scalingFactor,indexIndividual)
-                crossoverIndividual = self._differentialCrossoverOperation(crossoverRate,indexIndividual,mutantIndividual)
-                if (fitnessValue:=objectiveFunction(*crossoverIndividual)) <= self.fitnessValuesPopulation[indexIndividual]:
-                    self.population[indexIndividual] = crossoverIndividual
-                    self.fitnessValuesPopulation[indexIndividual] = fitnessValue
-        return self._optimalSolution(populationSize)
+            self.diffevol_IterativeSearch(iteration)
+        return self.diffevol_OptimalSolution(populationSize)
 
-    def _differentialMutantOperation(self,scalingFactor:float,indexIndividual:int):
+    def diffevol_InitializePopulation(self):
+        populationSize = self.populationSize
+        self.population = [self.initializeIndividual() for _ in range(populationSize)]
+        self.fitnessValuesPopulation = [self.objectiveFunction(*individual) for individual in self.population]
+
+    def diffevol_IterativeSearch(self,iteration:int):
+        populationSize = self.populationSize
+        for indexIndividual in range(populationSize):
+            mutantIndividual = self.diffevol_MutantOperation(indexIndividual)
+            crossoverIndividual = self.diffevol_CrossoverOperation(indexIndividual,mutantIndividual)
+            if (fitnessValue:=self.objectiveFunction(*crossoverIndividual)) <= self.fitnessValuesPopulation[indexIndividual]:
+                self.population[indexIndividual] = crossoverIndividual
+                self.fitnessValuesPopulation[indexIndividual] = fitnessValue
+
+    def diffevol_MutantOperation(self,indexIndividual:int):
         """
-            Private method to apply Differential Mutant Operation to an individual
+            Method to apply Differential Mutant Operation to an individual
         """
         pass
 
-    def _differentialCrossoverOperation(self,crossoverRate:float,indexIndividual:int,mutantIndividual):
+    def diffevol_CrossoverOperation(self,indexIndividual:int,mutantIndividual):
         """
-            Private method to apply Differential Crossover Operation 
+            Method to apply Differential Crossover Operation 
         """
         pass
     
-    def _optimalSolution(self,populationSize:int):
+    def diffevol_OptimalSolution(self,populationSize:int):
         """
-            Private method to return the optimal solution found
+            Method to return the optimal solution found
         """
         pass

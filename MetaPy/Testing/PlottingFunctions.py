85e53a0bc941095e2a43aec3f671bd7882a88d08
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-06-20 12:07:28 -0600

    Mod in testing and adds in plotting functions

--- MetaPy/Testing/PlottingFunctions.py
+++ MetaPy/Testing/PlottingFunctions.py
@@ -1,24 +1,26 @@
-def PlottingSnapshots(snapshots,intervals:int) -> None:
+def PlottingSnapshots(snapshots,intervals: int,SubPlot_kw: dict={'autoscale_on':False},Fig_kw: dict={'layout':'constrained'}) -> None:
     """
         Function to scatter plots of snapshots of the population
         -- snapshots : List of snapshots
         -- intervals : Every how many iterations being plotting
+        -- SubPlot_kw : Dict with keywords to config each subplot
+        -- Fig_kw : Dict with keywords to config the figure of plotting
     """
     import matplotlib.pyplot as plt
-    AmountPlots = len(snapshots)//intervals
+    AmountPlots = len(snapshots)//intervals+1
     RowsCanvas , ResidualPlots = divmod(AmountPlots,3) 
     if ResidualPlots>0:
         RowsCanvas += 1
-    Fig , Axs = plt.subplots(RowsCanvas,3,figsize=(12,4*RowsCanvas),layout='constrained',subplot_kw={'autoscale_on':False})
+    Fig , Axs = plt.subplots(RowsCanvas,3,figsize=(12,4*RowsCanvas),subplot_kw=SubPlot_kw,**Fig_kw)
     Axs = Axs.flat
     if len(snapshots[0])==2:
-        for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots):
+        for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots[::intervals]):
             Iteration , Population = Snapshot
             X , Y = Population[:,0] , Population[:,1]
             Axs[AxPosition].scatter(X,Y)
             Axs[AxPosition].set_title(f'Iteration : {Iteration}') 
     else:
-        for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots):
+        for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots[::intervals]):
             Iteration , Population , Clusters = Snapshot
             X , Y = Population[:,0] , Population[:,1]
             Axs[AxPosition].scatter(X,Y,c=Clusters)

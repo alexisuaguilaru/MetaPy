12f9c5bb1dad929807174bf6b9692933ed0eaa39
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-06-25 12:42:49 -0600

    Adds to handle in a better way plotting of clusters

--- Testing/PlottingFunctions.py
+++ Testing/PlottingFunctions.py
@@ -1,14 +1,16 @@
-def PlottingSnapshots(snapshots,intervals: int,SubPlot_kw: dict={'autoscale_on':False},Fig_kw: dict={'layout':'constrained'}) -> None:
+def PlottingSnapshots(snapshots,intervals: int,SubPlot_kw: dict={'autoscale_on':False},Fig_kw: dict={'layout':'constrained'}, Scatter_kw: dict={}) -> None:
     """
         Function to scatter plots of snapshots of the population
         -- snapshots : List of snapshots
         -- intervals : Every how many iterations being plotting
         -- SubPlot_kw : Dict with keywords to config each subplot
         -- Fig_kw : Dict with keywords to config the figure of plotting
+        -- Scatter_kw : Dict with keywords to config the scatter plotting
     """
     import matplotlib.pyplot as plt
     AmountPlots = len(snapshots)//intervals+1
-    RowsCanvas , ResidualPlots = divmod(AmountPlots,3) 
+    RowsCanvas , ResidualPlots = divmod(AmountPlots,3)
+
     if ResidualPlots>0:
         RowsCanvas += 1
     Fig , Axs = plt.subplots(RowsCanvas,3,figsize=(12,4*RowsCanvas),subplot_kw=SubPlot_kw,**Fig_kw)
@@ -17,11 +19,12 @@ def PlottingSnapshots(snapshots,intervals: int,SubPlot_kw: dict={'autoscale_on':
         for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots[::intervals]):
             Iteration , Population = Snapshot
             X , Y = Population[:,0] , Population[:,1]
-            Axs[AxPosition].scatter(X,Y)
+            Axs[AxPosition].scatter(X,Y,**Scatter_kw)
             Axs[AxPosition].set_title(f'Iteration : {Iteration}') 
+    
     else:
         for AxPosition , Snapshot in zip(range(3*RowsCanvas),snapshots[::intervals]):
             Iteration , Population , Clusters = Snapshot
             X , Y = Population[:,0] , Population[:,1]
-            Axs[AxPosition].scatter(X,Y,c=Clusters)
+            Axs[AxPosition].scatter(X,Y,c=Clusters,**Scatter_kw)
             Axs[AxPosition].set_title(f'Iteration : {Iteration}') 

37bd210399fbcc4bd37356c458e153624da9ee08
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-08-11 18:32:34 -0600

    ADD method to update vector a

--- MetaPy/GreyWolfOptimizer/GreyWolfOptimizer.py
+++ MetaPy/GreyWolfOptimizer/GreyWolfOptimizer.py
@@ -17,12 +17,13 @@ class GreyWolfOptimizer:
         import numpy as np
         self.populationSize = populationSize
         self.GWO_InitializePopulation()
+        self.GWO_
         vector_a = np.full(len(self.population[0]),2,dtype=np.float64)
-        functionDecreasedVector_a = lambda iteration: vector_a - 2*iteration/(iterations-1)
-        for iteration in range(iteration):
+        for iteration in range(iteration+1):
             vectorA = 
-            vectorC = 
-
+            vectorC =  
+            self.GWO_decreaseVector_a(vector_a,iteration,iterations)
+            
     def GWO_InitializePopulation(self):
         """
             Method to initialize population and fitnessValuesPopulation attributes  
@@ -30,3 +31,15 @@ class GreyWolfOptimizer:
         import numpy as np
         self.population = np.array([self.initializeIndividual() for _ in range(self.populationSize)])
         self.fitnessValuesPopulation = np.array([self.objectiveFunction(individual) for individual in self.population])
+
+    def GWO_decreaseVector_a(self,vector_a,iteration,iterations):
+        """
+            Method to linearly decrease the vector a's entries 
+        """
+        import numpy as np
+        if iteration == iterations-1:
+                vector_a = np.full(len(self.population[0]),0,dtype=np.float64)
+        else:
+            vector_a -= 2/iterations
+
+    
